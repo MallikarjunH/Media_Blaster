7f9535bac81edf958cf56b3a8cefd6f928c9e20c
MallikarjunH <mallikarjun.h1410@gmail.com>
2020-04-26 17:49:38 +0530

    Added Starter Project

---
+++ Media_Blaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Media_Blaster
+//
+//  Created by Mallikarjun on 26/04/20.
+//  Copyright © 2020 ADDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
